a8d0a20586c8c7eab91c396a69a19351a2a7036d
MuhammedAli21 <mohamedmohamed20211@gmail.com>
2024-10-22 20:40:31 +0300

    - Adding new Eng - Owner UI Screen.

--- lib/Config/routes/routes_manger.dart
+++ lib/Config/routes/routes_manger.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:pesticides/Features/register/presentation/pages/register_screen.dart';
 
+import '../../Features/eng_owner_screen/presentation/pages/eng_owner_screen.dart';
+
 class RoutesManger{
 static Map<String,Widget Function(BuildContext)>route={
   routeNameRegister:(context)=> RegisterScreen(),
+  routeNameEngOwnerScreen:(context)=> EngOwnerScreen(),
 };
 
 static const String routeNameRegister="register";
+static const String routeNameEngOwnerScreen="EngOwnerScreen";
 }
 
 
--- lib/Core/utils/images.dart
+++ lib/Core/utils/images.dart
@@ -1,3 +1,5 @@
 class ImageManager{
   static const String logoTeam="assets/images/logo_team.png";
+  static const String engIcon="assets/images/worker1 1.png";
+  static const String ownerIcon="assets/images/administrator1 1.png";
 }
